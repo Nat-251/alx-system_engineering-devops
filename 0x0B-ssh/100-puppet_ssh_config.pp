e8f5453d7bc8bcc112df4c3e4f9411f5423bdcc6
Nat-251 <natygizachew@gmail.com>
2023-07-19 00:20:14 +0300

    oldPro

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-# puppet file that edits the local host ssh client
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
+# Puppet script to create ssh config file
+ file_line { 'Turn off passwd auth':
+	ensure => 'present',
+     path   => '/etc/ssh/ssh_config',
+ line   => '    PasswordAuthentication no',
+       }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile /home/vagrant/.ssh/holberton',
-}
+       file_line { 'Declare identity file':
+         ensure => 'present',
+           path   => '/etc/ssh/ssh_config',
+            line   => '    IdentityFile ~/.ssh/school',
+             }
